3db893a2aac99f86a802f410da0da8780fd69e0a
fmwong77 <fongmeiw@gmail.com>
2020-06-16 22:35:30 -0500

    initial commit

---
+++ ProgramFlow/blocks.py
@@ -0,0 +1,15 @@
+# for i in range(1, 13):
+#     print("No. {} squared is {} and cubed is {:4}".format(i, i**2, i**3))
+# print("*" * 80)
+
+name = input("Please enter your name: ")
+age = int(input("How old are you, {}? ".format(name)))
+print(age)
+
+if age < 18:
+    print("Pleas come back in {0} years".format(18-age))
+elif age == 900:
+    print("Sorry, Yoda, you die in Return of the Jedi")
+else:
+    print("You're old enough to vote")
+    print("Please put an X in the box")
---
+++ ProgramFlow/truefalse.py
@@ -0,0 +1,8 @@
+day = "Monday"
+temperature = 30
+raining = False
+
+if (day == "Saturday" and temperature > 27) or not raining:
+    print("Go swimming")
+else:
+    print("Learn Python")
